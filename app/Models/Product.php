3b46c993bbb36e36fda9e677a327ac729a7119dc
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2021-04-01 18:01:19 +0900

    必要なモデル作成

---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'products';
+
+    protected $fillable = [
+        'company_id',
+        'product_name',
+        'price',
+        'stock',
+        'comment'
+    ];
+}
---
+++ app/Models/Sale.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model
+{
+    protected $table = 'sales';
+
+    protected $fillable = [
+        'product_id'
+    ];
+}
